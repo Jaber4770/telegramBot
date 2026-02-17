9ff2a02840be57a4adbd94fa036a16c5dab6e653
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-02-17 12:58:12 +0100

    telegram test bot created

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# it should be sectect but here i just keep it because its a practice project. 
+TOKEN = "7906215316:AAENYFoolZjNwtVaPIuXoXWr5712jBXY2do"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 হ্যালো! আমি তোমার Practice Bot")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "/start - Bot শুরু\n"
+        "/python - Python resource\n"
+        "/jack - make by jack sargey\n"
+        "/help - Help menu"
+    )
+
+async def python(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🐍 Python শেখার জন্য:\nhttps://docs.python.org/3/"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("python", python))
+
+print("🤖 Bot is running...")
+app.run_polling()
